7de6ce8187347a39f553e2b18d050b9e4fc20cb5
Hassan Alam <hassanalamdevs@gmail.com>
2020-10-27 18:28:01 +0500

    Function that returns true or false for orientation.

---
+++ isLandscape.js
@@ -0,0 +1,7 @@
+// Just a one liner function but, found a great best practice.
+isLandscape(1024, 1280);
+isLandscape(1024, 1024);
+isLandscape(1280, 1024);
+// Fallen in love with... 
+// The one liners
+function isLandscape(width, height) { return width > height; }
