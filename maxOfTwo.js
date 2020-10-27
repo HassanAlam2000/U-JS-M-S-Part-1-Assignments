257f77f5a416073c60f2a7c2a174eb2de6ba4e10
Hassan Alam <hassanalamdevs@gmail.com>
2020-10-27 18:33:36 +0500

    Removed bad comments from the code.

--- maxOfTwo.js
+++ maxOfTwo.js
@@ -1,7 +1,6 @@
-// Returns maximum of two...
-
-max(1, 2); // Output: 2
+max(1, 2); // Output: 2 
 max(2, 2); // Output: 2
 max(2, 1); // Output: 2
 
+// Returns maximum of two...
 function max(a, b) { return a > b ? a : b; }
